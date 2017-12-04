8edeb22c1ace6fb0949c6a8a7a89b41fcb79c8f6
Manuel Cortez <manuel@manuelcortez.net>
2017-12-04 17:02:08 -0600

    Defines some app data

---
+++ src/application.py
@@ -0,0 +1,9 @@
+from lyntin import constants
+
+name = "Mudder"
+version = "0.1"
+constants.STARTUPTEXT = """Initialization complete.
+Welcome to Mudder, version %s
+
+For help, type "#help general".
+""" % version
