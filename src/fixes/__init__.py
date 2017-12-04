be22fce26e8203c818f3bad6da0a96ca9e2108a7
Manuel Cortez <manuel@manuelcortez.net>
2017-12-04 04:54:32 -0600

    Added other components

---
+++ src/fixes/__init__.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+""" This module contains some bugfixes for packages used in TWBlue."""
+import sys
+import fix_win32com
+def setup():
+#	if hasattr(sys, "frozen"):
+	fix_win32com.fix()
---
+++ src/run.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python
+#######################################################################
+# This file is part of Lyntin.
+# copyright (c) Free Software Foundation 2001, 2002
+#
+# Lyntin is distributed under the GNU General Public License license.  See the
+# file LICENSE for distribution details.
+# $Id: runlyntin.pyw,v 1.1 2003/10/03 02:17:48 willhelm Exp $
+#######################################################################
+bootoptions = {"ui": "wx",
+               "datadir": "",
+               "moduledir": [],
+               "readfile": [],
+               "snoopdefault": 1}
+
+import sys
+import output
+import fixes
+fixes.setup()
+output.setup()
+if len(sys.argv) >= 2:
+  bootoptions["readfile"] = [sys.argv[1]]
+
+if __name__ == '__main__':
+  import lyntin.engine
+  lyntin.engine.main(bootoptions)
+
+# Local variables:
+# mode:python
+# py-indent-offset:2
+# tab-width:2
+# End:
+ 
