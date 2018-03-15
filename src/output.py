a38d871e13116d01d3c538f4eddda33a42a4e492
Manuel Cortez <manuel@manuelcortez.net>
2018-03-15 13:21:46 -0600

    Updated screenreading support

--- src/output.py
+++ src/output.py
@@ -1,42 +1,31 @@
 # *- coding: utf-8 -*-
 import logging as original_logging
-logger = original_logging.getLogger('core.output')
+logging = original_logging.getLogger('core.output')
 
 from accessible_output2 import outputs
 import sys
 
 speaker = None
-retries = 0
 
 def speak(text, interrupt=0):
- global speaker, retries
+ global speaker
  if not speaker:
   setup()
- try:
-  speaker.speak(text, interrupt)
- except:
-  if retries < 5:
-   retries = retries + 1
-   speak(text)
-# speaker.braille(text)
+ speaker.speak(text, interrupt)
+ speaker.braille(text)
 
 def setup ():
  global speaker
- logger.debug("Initializing output subsystem.")
- try:
-#  speaker = speech.Speaker(speech.outputs.Sapi5())
-#  else:
-  speaker = outputs.auto.Auto()
- except:
-  logger.exception("Output: Error during initialization.")
-
-def enable_sapi():
-  speaker = outputs.sapi.SAPI5()
+ logging.debug("Initializing output subsystem.")
+# try:
+ speaker = outputs.auto.Auto()
+# except:
+#  return logging.exception("Output: Error during initialization.")
 
 def copy(text):
  import win32clipboard
  #Copies text to the clipboard.
  win32clipboard.OpenClipboard()
  win32clipboard.EmptyClipboard()
- win32clipboard.SetClipboardText(text)
+ win32clipboard.SetClipboardText(text, win32clipboard.CF_UNICODETEXT)
  win32clipboard.CloseClipboard()
