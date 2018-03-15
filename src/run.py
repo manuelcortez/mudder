2df5a80883363de5a09a144e9ed3da68531b2a0e
Manuel Cortez <manuel@manuelcortez.net>
2018-03-15 13:23:10 -0600

    Added external modules dir as moduledir in lyntin's settings

--- src/run.py
+++ src/run.py
@@ -7,17 +7,22 @@
 # file LICENSE for distribution details.
 # $Id: runlyntin.pyw,v 1.1 2003/10/03 02:17:48 willhelm Exp $
 #######################################################################
+import sys
+import os
+import output
+from platform_utils import paths
+import logging
+logging.basicConfig(filename=os.path.join(paths.app_path(), "info.log"), filemode="w")
+import fixes
+fixes.setup()
+output.setup()
+
 bootoptions = {"ui": "wx",
                "datadir": "",
-               "moduledir": [],
+               "moduledir": [os.path.join(paths.app_path(), "modules")],
                "readfile": [],
                "snoopdefault": 1}
 
-import sys
-import fixes
-fixes.setup()
-import output
-output.setup()
 if len(sys.argv) >= 2:
   bootoptions["readfile"] = [sys.argv[1]]
 
